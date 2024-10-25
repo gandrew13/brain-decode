0897bdf18eb33e753ed5769b51fd3bac98c54a4f
gandrew13 <andrei.galbenus13@gmail.com>
2024-10-25 16:18:25 +0200

    Updated the runner to support running a job at a specific date.

--- scripts/runner.py
+++ scripts/runner.py
@@ -4,22 +4,23 @@ from datetime import datetime
 
 
 
-def run_job(hour: str, min: str, cmd: str) -> None:
+def run_job(day: str, hour: str, min: str, cmd: str) -> None:
     '''
-    Runs a command line command at a specific time
+    Runs a command line command at a specific time.
     '''
+    day = int(day)
     hour = int(hour)
     min = int(min)
 
     while True:
         now = datetime.now()
-        if now.hour == hour and now.minute == min:
+        if now.day == day and now.hour == hour and now.minute == min:
             subprocess.run(cmd, shell=True)
             return
 
 
 def main():
-    if len(sys.argv) == 4:
+    if len(sys.argv) == 5:      # [runner.py day hour minute cmd]
         run_job(*sys.argv[1:])
     else:
         print("Error: invalid number of arguments!")
