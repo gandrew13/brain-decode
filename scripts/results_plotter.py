3c40757cf796cd0e56063b94570a60cded2167db
gandrew13 <andrei.galbenus13@gmail.com>
2024-11-06 15:15:28 +0100

    Updated results plotter to ignore header and last entry (which represents the loss over the entire dataset).

--- scripts/results_plotter.py
+++ scripts/results_plotter.py
@@ -4,21 +4,25 @@ from typing import Tuple
 import numpy as np
 import matplotlib.pyplot as plt
 
+def is_csv_header(csv_line: str) -> bool:
+    return ("step" in csv_line and "loss" in csv_line)
 
 
-def parse_results_file(results_file: str) -> Tuple[np.array, np.array]:
+def parse_results_file(results_file: str, train_results: bool = False) -> Tuple[np.array, np.array]:
     with open(results_file, mode ='r')as f:
         data = csv.reader(f)
-        next(data)                      # skip first (header) line
         steps = np.array([])
         losses = np.array([])
-        [(steps := np.append(steps, int(line[0])), losses := np.append(losses, float(line[1]))) for line in data]
+        [(steps := np.append(steps, int(line[0])), losses := np.append(losses, float(line[1]))) for line in data if not is_csv_header(line)]
+    
+    if train_results:
+        steps, losses = steps[:-1], losses[:-1]     # last point represents the full loss over the entire dataset, so remove it from the graph
     return steps, losses
     
 
 
 def plot(train_results_file: str, eval_resuls_file: str) -> None:
-    train_steps, train_losses = parse_results_file(train_results_file)
+    train_steps, train_losses = parse_results_file(train_results_file, train_results=True)
     eval_steps, eval_losses =  parse_results_file(eval_resuls_file)
     
     plt.plot(train_steps, train_losses, label = "train loss")
