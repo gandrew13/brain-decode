937cc9be06f12066b9ec55a1a56f126c79d7f813
gandrew13 <andrei.galbenus13@gmail.com>
2024-11-29 11:11:10 +0100

    Added model wrapper class implemeting PyTorch Lightning's callbacks.

---
+++ scripts/model_wrapper.py
@@ -0,0 +1,152 @@
+
+
+from torch import optim, cuda, save, load
+from torch.nn import CrossEntropyLoss
+import lightning as L
+from torchmetrics.classification import Accuracy
+import braindecode.models as models
+import torch
+
+class LModelWrapper(L.LightningModule):
+    '''
+    PyTorch Lightning wrapper class. 
+    '''
+    def __init__(self, model, pretrained_model, num_classes, num_channels, final_fc_length):
+        super().__init__()
+
+        match model:
+            case "eegconformer":
+                self.model = models.EEGConformer(num_classes, num_channels, final_fc_length=final_fc_length, add_log_softmax=False) # we use CrossEntropy loss, so no need to add a LogSoftmax layer
+            case "eegnet":
+                self.model = models.EEGNetv4(num_channels, num_classes, 400)
+            case "mlp":
+                self.model = torch.nn.Sequential(torch.nn.Flatten(),
+                                                 torch.nn.Linear(6600, 256),
+                                                 torch.nn.BatchNorm1d(256),
+                                                 torch.nn.ReLU(),
+                                                 torch.nn.Dropout1d(0.5),
+                                                 torch.nn.Linear(256, 128),
+                                                 torch.nn.BatchNorm1d(128),
+                                                 torch.nn.ReLU(),
+                                                 torch.nn.Dropout1d(0.5),
+                                                 torch.nn.Linear(128, num_classes))
+            case _:
+                print("Error: Unknown model!")
+
+        if pretrained_model:
+            self.model.load_state_dict(load(pretrained_model))
+
+        # TODO: freeze all except linear layers
+        #for name, params in self.model.named_parameters():
+        #    if "fc" not in name:
+        #        params.requires_grad = False
+
+        self.loss = CrossEntropyLoss()
+        self.eval_loss = CrossEntropyLoss()
+        self.accuracy = Accuracy(task="multiclass", num_classes=num_classes)
+
+        self.epoch_loss = 0.0
+
+        self.prev_losses = {"prev_train_loss": 10000.0, "prev_eval_loss": 100000.0}
+
+        #self.batch_results = {"train_batch_logits": [], "train_batch_labels": [], "valid_batch_logits": [], "valid_batch_labels": []}
+
+        self.my_log_dict = {"epoch": -1, "train_loss": 1000.0, "eval_loss": 1000.0}
+
+        self.batch_logits = []
+        self.batch_labels = []
+
+
+    def training_step(self, batch, batch_idx):
+        assert self.model.training
+        self.step(batch)
+        loss = self.loss(self.batch_logits[-1], self.batch_labels[-1])
+
+        self.epoch_loss += loss.item()
+        #self.my_log(loss.item())
+
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        assert not self.model.training
+        self.step(batch)
+        loss = self.eval_loss(self.batch_logits[-1], self.batch_labels[-1])
+        self.epoch_loss += loss.item()
+
+    def test_step(self, batch, batch_idx):
+        assert not self.model.training
+        self.step(batch)
+
+    def configure_optimizers(self):
+        return optim.AdamW(self.parameters(), lr=1e-3)
+        #return optim.SGD(self.parameters(), lr=0.01, weight_decay=0.001, momentum=0.9)
+        #return optim.SGD(self.parameters(), lr=1e-2, weight_decay=1e-3, momentum=0.9)
+    
+    def my_log(self, values_dict):
+        self.log_dict(values_dict, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        if self.current_epoch % 10 == 0:
+            pass
+            #self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+            #self.log("acc", self.accuracy(), on_step=False, on_epoch=True, prog_bar=True, logger=True)
+
+            # save model
+
+    def on_train_epoch_end(self):
+        # This executes after validation_step and on_validation_epoch_end
+        self.my_log(self.my_log_dict)
+
+    def on_validation_epoch_start(self):
+        mean_loss = round(self.epoch_loss / self.trainer.num_training_batches, 3)
+        print("\nMean loss:", mean_loss)
+        self.my_log_dict["epoch"] = int(self.current_epoch)
+        self.my_log_dict["train_loss"] = mean_loss
+        self.save_model()
+
+        self.epoch_loss = 0.0
+        
+        if self.current_epoch == (self.trainer.max_epochs - 1):
+            # compute accuracy on last epoch
+            print("\nTrain Accuracy:", self.compute_accuracy())
+
+        self.batch_logits = []
+        self.batch_labels = []
+
+
+    def on_validation_epoch_end(self):
+        assert not self.model.training
+        print("\nValidation Accuracy:", self.compute_accuracy(), "\n")
+        mean_loss = round(self.epoch_loss / self.trainer.num_val_batches[0], 3)
+        self.my_log_dict["eval_loss"] = mean_loss
+        self.epoch_loss = 0.0
+
+    def on_test_epoch_end(self):
+        assert not self.model.training
+        if self.current_epoch == (self.trainer.max_epochs - 1):
+            print("Test Accuracy:", self.compute_accuracy())
+
+    def step(self, batch):
+        '''
+        Process (forward propagate) a batch.
+        '''
+        inputs, labels = batch
+        inputs, labels = inputs.type(cuda.FloatTensor), labels.type(cuda.LongTensor)
+        out = self.model(inputs)
+        self.batch_logits.append(out)
+        self.batch_labels.append(labels)
+
+    def compute_accuracy(self):
+        self.model.eval()
+        logits = torch.argmax(torch.cat(self.batch_logits), dim=1)
+        labels = torch.cat(self.batch_labels)
+
+        self.batch_logits = []
+        self.batch_labels = []
+
+        return round(self.accuracy(logits, labels).item(), 3)
+    
+    def save_model(self):
+        if self.current_epoch % 10 == 0:
+            if self.prev_losses["prev_train_loss"] > self.epoch_loss:
+                print("Saving model...")
+                save(self.model.state_dict(), "scripts/best_train_loss.pth")
+                self.prev_losses["prev_train_loss"] = self.epoch_loss
