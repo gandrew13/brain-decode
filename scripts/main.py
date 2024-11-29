692ea846317d2b9ace9e9dd3def1014d2c5d9290
gandrew13 <andrei.galbenus13@gmail.com>
2024-11-29 10:48:41 +0100

    Added more command line args and moved some code to the Runner class.

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,5 @@
 import argparse
-
-from Datasets.EEGImageNet.ds_preprocess import EEGImageNetDataset # type: ignore
-
+from model_runner import Runner
 
 
 def data_prep(args: str):
@@ -9,7 +7,8 @@ def data_prep(args: str):
     # take each file and preprocess it
     # save the preprocessed_data
     
-    ds = EEGImageNetDataset(args)
+    #ds = EEGImageNetDataset(args)
+    pass
 
 
 def train():
@@ -25,15 +24,19 @@ def infer():
 
 def parse_args() -> str:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dataset_dir", required=True, help="Path to the datasets folder.")
-    parser.add_argument("-g", "--granularity", required=False, help="choose from coarse, fine0-fine4 and all")
-    parser.add_argument("-m", "--model", required=False, help="model")
-    parser.add_argument("-b", "--batch_size", default=40, type=int, help="batch size")
-    parser.add_argument("-p", "--pretrained_model", help="pretrained model")
-    parser.add_argument("-s", "--subject", default=0, type=int, help="subject from 0 to 15")
-    parser.add_argument("-o", "--output_dir", required=False, help="directory to save results")
+    parser.add_argument("-d", "--dataset", required=True, help="Dataset name: [eegimagenet, alljoined1, bci_iv_2a, 52sub_64ch_2class]")
+    parser.add_argument("-dp", "--dataset_path", required=False, help="Path of the dataset file")
+    parser.add_argument("-m", "--model", required=True, help="Model name: [eegconformer, eegnet, mlp]")
+    parser.add_argument("-l", "--load_pretrained", required=False, help="Path to the pretrained model")
+    parser.add_argument("-p", "--plot_file", required=False, help="Path to the results file to plot")
+    #parser.add_argument("-g", "--granularity", required=False, help="choose from coarse, fine0-fine4 and all")
+    #parser.add_argument("-m", "--model", required=False, help="model")
+    #parser.add_argument("-b", "--batch_size", default=40, type=int, help="batch size")
+    #parser.add_argument("-p", "--pretrained_model", help="pretrained model")
+    #parser.add_argument("-s", "--subject", default=0, type=int, help="subject from 0 to 15")
+    #parser.add_argument("-o", "--output_dir", required=False, help="directory to save results")
     
-    return parser.parse_args()  
+    return parser.parse_args()
 
 
 def main():
@@ -41,9 +44,13 @@ def main():
     
     args = parse_args()
 
-    data_prep(args)
-    train()
-    infer()
+    runner = Runner(args)
+    runner.run()
+    runner.plot(args.plot_file)
+
+    #data_prep(args)
+    #train()
+    #infer()
 
 
 main()
