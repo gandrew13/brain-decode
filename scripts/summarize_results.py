90f7843b7c45ccd257cfc7658e9c2e9775c6bc04
gandrew13 <andrei.galbenus13@gmail.com>
2025-04-17 09:53:54 +0200

    Updated results parser to output the mean and standard deviation over runs.

--- scripts/summarize_results.py
+++ scripts/summarize_results.py
@@ -6,24 +6,25 @@ def parse_results(folder):
     for file in glob.glob(folder + "*.txt"):
         with open(file) as f:
             content = f.read()
-            acc_idx = content.find("Test Accuracy: ")
-            if acc_idx != -1:
-                res.append(file + ": " + content[acc_idx:acc_idx + len("Test Accuracy: ") + 5].rstrip("\n"))
+            mean_idx = content.find("Mean: ")
+            if mean_idx != -1:
+                res.append(file.split('/')[-1] + ": " + content[mean_idx:mean_idx + len(": ") + 5 + len("Std dev: ") + 25].rstrip("\n"))
             else:
-                res.append("Test accuracy not found in: " + file)
+                res.append("Mean and std dev not found in: " + file)
     return res
 
 
 def print_results():
-    nopretrain_res = parse_results("outputs_nopretrain/")
-    pretrain_res = parse_results("outputs_pretrain/")
+    nopretrain_res = parse_results("results/outputs_nopretrain/")
+    pretrain_res_fclayers = parse_results("results/outputs_pretrain_fclayers/")
+    pretrain_res_fullmodel = parse_results("results/outputs_pretrain_fullmodel/")
+    #pretrain_res_transformer_fclayers = parse_results("results/outputs_pretrain_transformer_fclayers/")
 
-    if len(nopretrain_res) != len(pretrain_res):
+    if len(nopretrain_res) != len(pretrain_res_fclayers):
         print("Error: nopretrain and pretrain number of subjects differ!")
 
-    for (el1, el2) in zip(nopretrain_res, pretrain_res):
-        print(el1, "       ", el2)
+    for (el1, el2, el3) in zip(nopretrain_res, pretrain_res_fullmodel, pretrain_res_fclayers):
+        print(el1, "       ", el2, "        ", el3,)
 
 
 print_results()
-
